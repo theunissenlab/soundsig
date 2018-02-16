0334ee17343d2e59acebb022174aa0e2da1e7615
Frederic Theunissen <theunissen@berkeley.edu>
2018-02-16 12:18:10 -0800

    Updating Setup for Pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name='soundsig',
-    version='0.5',
-    packages=['soundsig',],
-    license='',
-    long_description=open('README.md').read(),
+    name = 'soundsig',
+    version = '0.1',
+    packages = ['soundsig'],
+    license = '',
+    description = 'Sound and Signal Analysis Tools for Bioacousticians and Auditory Neurophysiologists',
+    long_description = open('README.md').read(),
+    author = 'Frederic Theunissen',
+    author_email = 'theunissen@berkeley.edu',
+    url = 'https://github.com/theunissenlab/soundsig', # use the URL to the github repo
+    download_url = 'https://github.com/theunissenlab/soundsig/archive/0.1.tar.gz',
+    keywords = [ 'bioacoustics', 'biosound', 'modulation power spectrum'],
     install_requires=['numpy',
                       'scipy',
                       'matplotlib',
